da791b4cc8a24f0ee192c2e83d31c8cc3ff3acf1
Aymen Guedri <aymenguedri01@gmail.com>
2024-10-16 23:29:26 +0100

    fix navigation

--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,8 @@
 export const navItems = [
   { name: "Home", link: "#" },
   { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
   { name: "Experience", link: "#experience" },
+  { name: "Projects", link: "#projects" },
   { name: "Contact", link: "#contact" },
 ];
 
