06a7add265961981704792b29429a5e654c4a2ee
aymen guedri <aymenguedri01@gmail.com>
2026-02-16 11:32:17 +0100

    fix icons mobile view

--- components/RecentProjectsMobile.tsx
+++ components/RecentProjectsMobile.tsx
@@ -2,17 +2,68 @@
 
 import { FaLocationArrow } from "react-icons/fa6";
 import { projects } from "@/data";
+import { getTranslatedData } from "@/data/translatedData";
 import { PinContainer } from "./ui/Pin";
+import { useTranslation } from "react-i18next";
+import {
+  FaReact,
+  FaNodeJs,
+  FaMicrochip,
+  FaYoutube,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiTypescript,
+  SiThreedotjs,
+  SiMongodb,
+  SiArduino,
+  SiRaspberrypi,
+  SiMqtt,
+  SiWordpress,
+  SiPhp,
+  SiFigma,
+  SiIonic,
+  SiFirebase,
+  SiStripe,
+} from "react-icons/si";
+
+const techIcons: { [key: string]: JSX.Element } = {
+  "/re.svg": <FaReact />,
+  "/tail.svg": <SiTailwindcss />,
+  "/ts.svg": <SiTypescript />,
+  "/three.svg": <SiThreedotjs />,
+  "/mongo.png": <SiMongodb />,
+  "/node.png": <FaNodeJs />,
+  "/arduino.png": <SiArduino />,
+  "/iot.png": <FaMicrochip />,
+  "/mqtt.png": <SiMqtt />,
+  "/rasberry.png": <SiRaspberrypi />,
+  "/Youtube_logo.png": <FaYoutube />,
+  "/wordpress.svg": <SiWordpress />,
+  "/php.svg": <SiPhp />,
+  "/figma.svg": <SiFigma />,
+  "/ionic.svg": <SiIonic />,
+  "/firebase.svg": <SiFirebase />,
+  "/stripe.svg": <SiStripe />,
+};
 
 const RecentProjectsMobile = () => {
+  const { t, i18n } = useTranslation();
+  const translatedData = getTranslatedData(i18n.language);
+  
+  const mergedProjects = projects.map((proj, index) => ({
+    ...proj,
+    ...translatedData.projects[index]
+  }));
+
   return (
-    <div className="py-20">
+    <div className="py-20" key={i18n.language}>
       <h1 className="heading">
-        A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        {t('projects.title')}{" "}
+        <span className="text-purple">{t('projects.recent')}</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
-        {projects.map((item) => (
+        {mergedProjects.map((item) => (
           <div
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
@@ -54,16 +105,17 @@ const RecentProjectsMobile = () => {
               </p>
 
               <div className="flex items-center justify-between mt-7 mb-3">
-                <div className="flex items-center">
-                  {item.iconLists.map((icon, index) => (
+                <div className="flex items-center space-x-2">
+                  {item.iconLists.map((icon: string, index: number) => (
                     <div
                       key={index}
                       className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{
-                        transform: `translateX(-${5 * index + 2}px)`,
-                      }}
                     >
-                      <img src={icon} alt="icon5" className="p-2" />
+                      {icon === "/unity.jpg" ? (
+                        <img src={icon} alt={`icon${index}`} className="p-2" />
+                      ) : (
+                        techIcons[icon as keyof typeof techIcons]
+                      )}
                     </div>
                   ))}
                 </div>
@@ -76,7 +128,7 @@ const RecentProjectsMobile = () => {
                     className="flex justify-center items-center"
                   >
                     <p className="sm:text-sm md:text-sm text-xs font-light text-purple">
-                      Check details
+                      {t('projects.checkDetails')}
                     </p>
                     <FaLocationArrow className="ms-3" color="#CBACF9" />
                   </a>
